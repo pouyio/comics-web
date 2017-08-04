77ec29fd0da325efd1683f4030cb43427ab0b5e0
Vicente <pouyio@gmail.com>
2017-08-04 18:56:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
+import { ResolveService } from './resolve.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  noBack = false;
+  logged = true;
+  resolving: Observable<boolean>;
+  subscription: Subscription;
+  location: Location;
+
+  constructor(
+    location: Location,
+    private router: Router,
+    private api: ApiService,
+    private auth: AuthService,
+    private resolver: ResolveService,
+    private cdRef :ChangeDetectorRef
+  ) {
+    router.events.subscribe(params =>  this.noBack = (router.url === '/login'));
+    this.logged = !!this.auth.getToken();
+  }
+
+  logout = () => {
+    this.logged = false;
+    this.api.logout();
+  }
+
+  ngOnInit() {
+    this.resolving = this.resolver.getState();
+    this.cdRef.detectChanges();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AuthGuard } from './auth-guard';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
+import { ResolveService } from './resolve.service';
+
+import { ComicIssueResolve } from './comic-issue-resolve';
+import { ComicsReadResolve } from './comics-read-resolve';
+import { ComicResolve } from './comic-resolve';
 
 import { AppComponent } from './app.component';
+import { ComicComponent } from './comic/comic.component';
+import { ComicIssueComponent } from './comic-issue/comic-issue.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { LoginComponent } from './login/login.component';
+
+import {HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+import {AppRoutes} from './app.routes';
+import { NewsComponent } from './news/news.component';
+
+declare var Hammer: any;
+
+export class MyHammerConfig extends HammerGestureConfig {
+  buildHammer(element: HTMLElement) {
+    let mc = new Hammer(element, {
+      touchAction: "auto",
+    });
+
+    // mc.get('pinch').set({enable: true});
+    return mc;
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComicComponent,
+    ComicIssueComponent,
+    SearchComponent,
+    HomeComponent,
+    ImageViewerComponent,
+    LoginComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(AppRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    ApiService,
+    ResolveService,
+    ComicResolve,
+    ComicsReadResolve,
+    ComicIssueResolve,
+    { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
