2bad8ddc3556e0422d660edfa04b439c93e62bb8
Vicente <pouyio@gmail.com>
2017-09-03 21:31:36 +0200

    implemented last page read for an issue

--- src/app/comic-issue/issue-presentation/issue-presentation.component.ts
+++ src/app/comic-issue/issue-presentation/issue-presentation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-issue-presentation',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssuePresentationComponent implements OnInit {
 
+  @Input() issue;
+  @Output() pageRead = new EventEmitter();
+  page: number = 0;
+  lastPage: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    if(this.issue) {
+      this.page = this.issue.info.page;
+      this.lastPage = this.issue.pages.length - 1;
+    }
   }
+
+  setPage = (page) => {
+    this.page = page;
+    this.pageRead.emit(this.page);
+  }
+
+  onPageChange = (advance) => {
+    this.page += advance;
+    this.setPage(this.page);
+  }
+
+  onChange = (page) => this.setPage(page);
 
 }
