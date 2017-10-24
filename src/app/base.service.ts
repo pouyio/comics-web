d27ee22f4775b8f180abcf87efeefcc96ecfc7d1
Vicente <pouyio@gmail.com>
2017-10-24 20:57:09 +0200

    url

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -9,9 +9,9 @@ import { ResolveService } from './resolve.service';
 @Injectable()
 export class BaseService {
 
-  // protected baseUrl: string = 'http://192.168.1.33:8080';
-  protected baseUrl: string = 'http://localhost:8080';
-  // protected baseUrl: string = 'http://ec2-52-57-163-72.eu-central-1.compute.amazonaws.com:8080';
+  // protected baseUrl: string = 'http://192.168.1.34:8080';
+  // protected baseUrl: string = 'http://localhost:8080';
+  protected baseUrl: string = 'http://ec2-52-57-163-72.eu-central-1.compute.amazonaws.com:8080';
 
   constructor(protected http: HttpClient, protected auth: AuthService, protected resolver: ResolveService) {}
 
