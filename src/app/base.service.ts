2d6e79323cf8182db8e5c2324684cd496dd91b05
Vicente <pouyio@gmail.com>
2017-12-25 17:06:34 +0100

    new endpoint

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -10,7 +10,7 @@ import { ResolveService } from './resolve.service';
 export class BaseService {
 
   // protected baseUrl: string = 'http://192.168.1.33:8080';
-  protected baseUrl: string = 'https://comic.vicenteortiz.me';
+  protected baseUrl: string = 'https://comic.vicenteortiz.me/comic';
 
   constructor(protected http: HttpClient, protected auth: AuthService, protected resolver: ResolveService) {}
 
