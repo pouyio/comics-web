02fd692675ac13cc9eef6bf5fedf5a9be0bedff6
Vicente <pouyio@gmail.com>
2018-01-27 00:14:03 +0100

    switch to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://comic.vicenteortiz.me:8084/graphql',
+  api_url: 'https://comic.vicenteortiz.me:8084/graphql',
   rest_url: 'https://comic.vicenteortiz.me:8081'
 };
