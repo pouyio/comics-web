7c9ffa9668cac455d20d8788b1e8e48e36459307
Vicente <pouyio@gmail.com>
2018-01-27 00:40:00 +0100

    fixed login endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api_url: 'https://comic.vicenteortiz.me:8084/graphql',
-  rest_url: 'https://comic.vicenteortiz.me:8081'
+  rest_url: 'https://comic.vicenteortiz.me:8084'
 };
