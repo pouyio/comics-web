40c40c73fb1d4f54b396c97d4b33744279b25b84
Vicente <pouyio@gmail.com>
2019-03-07 19:14:00 +0100

    trying new things

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index/graphql',
-  rest_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index'
+  // api_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index/graphql',
+  // rest_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index'
+  api_url: 'https://comic-graphql-ohv6ifyvl.now.sh/graphql',
+  rest_url: 'https://comic-graphql-ohv6ifyvl.now.sh'
 };
