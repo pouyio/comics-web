43f8e21dcc9a27dc29efcd51293cae40ab146dd0
Vicente <pouyio@gmail.com>
2019-03-07 19:56:03 +0100

    moved to now

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // api_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index/graphql',
-  // rest_url: 'https://hardcore-brown-51dad3.netlify.com/.netlify/functions/index'
-  api_url: 'https://comic-graphql-ohv6ifyvl.now.sh/graphql',
-  rest_url: 'https://comic-graphql-ohv6ifyvl.now.sh'
+  api_url: 'https://comics-graphql.vicenteortiz.me/graphql',
+  rest_url: 'https://comics-graphql.vicenteortiz.me/'
 };
