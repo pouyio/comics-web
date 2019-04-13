3db4eb3e07c76b4963017b4e14392db29edd6dec
Vicente <pouyio@gmail.com>
2019-04-13 18:48:57 +0200

    fix endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api_url: 'https://comics-graphql.vicenteortiz.me/graphql',
-  rest_url: 'https://comics-graphql.vicenteortiz.me/'
+  rest_url: 'https://comics-graphql.vicenteortiz.me'
 };
