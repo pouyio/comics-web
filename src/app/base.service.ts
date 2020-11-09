6d172ff29288dac379e7b8a8a4e0c2a5e8e89f18
pouyio <pouyio@gmail.com>
2020-11-09 20:51:27 +0100

    removes unused dep

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Response } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
-import { AuthService } from './auth.service';
-import { ResolveService } from './resolve.service';
-import { environment } from '../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { of } from "rxjs";
+import { AuthService } from "./auth.service";
+import { ResolveService } from "./resolve.service";
+import { environment } from "../environments/environment";
 
 @Injectable()
 export class BaseService {
-
   protected baseUrl: String = environment.rest_url;
 
-  constructor(protected http: HttpClient, protected auth: AuthService, protected resolver: ResolveService) {}
+  constructor(
+    protected http: HttpClient,
+    protected auth: AuthService,
+    protected resolver: ResolveService
+  ) {}
 
-  protected handleError (error: Response | any) {
+  protected handleError(error: HttpErrorResponse) {
     // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    if (error.error instanceof ErrorEvent) {
+      errMsg = `${error.status} - ${error.error.message || ""}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
