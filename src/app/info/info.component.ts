0be24306428944bd23c3a142fa88f4cd51ac678e
pouyio <pouyio@gmail.com>
2020-11-10 21:35:39 +0100

    fixed execution

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,59 +1,60 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import gql from 'graphql-tag';
-import { Apollo } from 'apollo-angular';
-import { tap, share } from 'rxjs/operators';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import gql from "graphql-tag";
+import { Apollo } from "apollo-angular";
+import { tap, share } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'pou-info',
-  templateUrl: './info.component.html',
+  selector: "pou-info",
+  templateUrl: "./info.component.html",
   styles: [
     `
-    .big-text {
-      font-size: 2.8rem;
-    }
-    `
+      .big-text {
+        font-size: 2.8rem;
+      }
+    `,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoComponent implements OnInit {
-
-  $info: any;
+  $info: Observable<any>;
   showTotal = true;
   showTime = true;
   daysSinceLastUpdate: string;
 
-  private infoQuery = gql`{
-    info {
-      last_update
-      genres
-      writers
-      publishers
-      artists
-      issues
-      comics {
-        completed
-        ongoing
+  private infoQuery = gql`
+    {
+      info {
+        last_update
+        genres
+        writers
+        publishers
+        artists
+        issues
+        comics {
+          completed
+          ongoing
+        }
       }
     }
-  }  
   `;
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
-    this.$info = this.apollo.query({ query: this.infoQuery })
-      .pipe(share(),
-      tap(({ data }: any) => this.setDaysSinceLastUpdate(data.info.last_update)));
+    this.$info = this.apollo.query({ query: this.infoQuery }).pipe(
+      share(),
+      tap(({ data }: any) => this.setDaysSinceLastUpdate(data.info.last_update))
+    );
   }
 
   setDaysSinceLastUpdate(date) {
-    const duration = (new Date().getTime() - new Date(date).getTime());
+    const duration = new Date().getTime() - new Date(date).getTime();
     const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-    const days = Math.floor((duration / (1000 * 60 * 60 * 24)));
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
 
-    this.daysSinceLastUpdate = days ?
-      `${days} days and ${hours} hours` :
-      `${hours} hours`;
+    this.daysSinceLastUpdate = days
+      ? `${days} days and ${hours} hours`
+      : `${hours} hours`;
   }
-
 }
